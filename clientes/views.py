9b0ccabe949d5964698f0c194049593d0745c340
af0liveira <af0liveira@icloud.com>
2023-07-25 16:56:00 +0200

    Add ordering `Cliente` view by `nome` in the API.
    On the API interface, Cliente items can now be ordered by the attribute
    'nome'.

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 
@@ -6,4 +8,5 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering_fields = ('nome',)
